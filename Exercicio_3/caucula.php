75fbf84d4e3b3c76adb9e14525e23357a981be9d
LauraVictoria20 <lauravianadasilva232@gmail.com>
2026-01-13 08:47:57 -0300

    adicionando formulário de controle

---
+++ Exercicio_3/caucula.php
@@ -0,0 +1,23 @@
+
+<!--Lógica de Verificação-->
+
+<?php
+
+
+$nome = $_POST['nome'];
+$tarefas = $_POST['tarefas'];
+$atrasos = $_POST['atrasos'];
+
+if(isset($_POST['tarefas'])){
+    if($_POST['tarefas'] >=  20 && $_POST['atrasos'] <= 1 ){
+     echo "Desempenho Excelente";
+    }
+    else if($_POST['tarefas'] == 10 || $_POST['tarefas'] >=19 && $_POST['atrasos'] >= 2 || $_POST['atrasos'] == 3){
+     echo "Desempenho Regular!" ;
+    }
+    else{
+     echo "Desempenho Insatisfatório!";
+  }
+}
+
+?>
