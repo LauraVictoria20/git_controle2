94ab1dd84d4983ab46a134b7e1357f6f66dbd42c
LauraVictoria20 <lauravianadasilva232@gmail.com>
2026-01-20 09:37:03 -0300

    alterando regra de desempenho excelente

--- Exercicio_3/caucula.php
+++ Exercicio_3/caucula.php
@@ -8,7 +8,7 @@ $tarefas = $_POST['tarefas'];
 $atrasos = $_POST['atrasos'];
 
 if(isset($_POST['tarefas'])){
-    if($_POST['tarefas'] >=  20 && $_POST['atrasos'] == 1 ){
+    if($_POST['tarefas'] >=  20 && $_POST['atrasos'] == 0 ){
      echo "Desempenho Excelente";
     }
     else if($_POST['tarefas'] == 10 || $_POST['tarefas'] >=19 && $_POST['atrasos'] >= 2 || $_POST['atrasos'] == 3){
